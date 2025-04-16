7ee341b6779272fb49df2f70e8bc9130d0a608f3
nryzhikh <nikita.ryzhikh@outlook.com>
2025-04-16 23:34:55 +0200

    auto commit

--- models/ots_all.sql
+++ models/ots_all.sql
@@ -1,15 +1,22 @@
 SELECT
     id,
-    (
-        COALESCE(CAST(otc_колво_подписчиков_канала AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_18 AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_45_inc_3_5 AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_54_bc AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_55 AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_55_bc AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_55_inc_3_5 AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_25_58_inc_3_5 AS NUMERIC), 0) +
-        COALESCE(CAST(ots_all_50_65 AS NUMERIC), 0) +
-        COALESCE(CAST(отs AS NUMERIC), 0)
+    (   
+       COALESCE(CAST(otc_колво_подписчиков_канала AS NUMERIC), 0) +
+        COALESCE(CAST(отs AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_18 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_44_inc_3_5 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_45 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_45_inc_3_5 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_54_bc AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_55 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_55_bc AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_55_inc_3_5 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_58_inc_3_5 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_60 AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_25_60_c AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_30_60_c AS NUMERIC), 0) +
+       COALESCE(CAST(ots_all_50_65 AS NUMERIC), 0)
+       
     ) AS ots_all
 FROM {{ ref('эконометрика_общая') }}
+
